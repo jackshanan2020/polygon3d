55f314b99696ab76204e55cc2caea4476f3bff08
Sahithyan <sahithyan2701@gmail.com>
2021-06-21 17:35:50 +0530

    footer links spacing fixed

--- src/components/footers/FiveColumnWithBackground.js
+++ src/components/footers/FiveColumnWithBackground.js
@@ -9,9 +9,12 @@ import { ReactComponent as LinkedinIcon } from "images/linkedin-icon.svg";
 import { ReactComponent as InstagramIcon } from "images/instagram-icon.svg";
 import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-9.svg";
 
-const Container = tw.div`relative bg-primary-500 text-gray-100 -mb-8 -mx-8 px-8 py-20 lg:py-24`;
-const Content = tw.div`max-w-screen-xl mx-auto relative z-10`;
-const FiveColumns = tw.div`flex flex-wrap text-center sm:text-left justify-center sm:justify-start md:justify-between -mt-12`;
+const Container = tw.div`relative bg-primary-500 text-gray-100 -mb-8 -mx-8 px-8 pt-6 pb-16 lg:pt-8 lg:pb-24`;
+const Content = tw.div`flex flex-col max-w-screen-xl mx-auto relative z-10`;
+const FiveColumns = tw.div`flex flex-col flex-wrap sm:flex-row
+  w-full sm:w-4/5
+  text-center sm:text-left
+  self-center justify-center sm:justify-start md:justify-between`;
 
 const Column = tw.div`px-4 sm:px-0 sm:w-1/3 md:w-auto mt-12`;
 
